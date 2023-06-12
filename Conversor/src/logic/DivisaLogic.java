d9c6d12d05a1a8603a956e65381222ade985bb80
Mathias Ledesma <romanmathias21@gmail.com>
2023-06-12 00:06:51 -0300

    se termino la interfaz de conversion de divisa

--- Conversor/src/logic/DivisaLogic.java
+++ Conversor/src/logic/DivisaLogic.java
@@ -2,9 +2,9 @@ package logic;
 
 public class DivisaLogic {
 	//los valores asignados a las divisas son su equivalente en pesos argentinos
-	private final double dolar = 244.37; //el valor corresponde al dolar blue, no al dolar convencional
+	private final double dolar = 483.37; //el valor corresponde al dolar blue, no al dolar convencional
 	private final double euro = 263.34;
-	private final double libraEsterlina = 306.82;
+	private final double libraEsterlina = 307.92;
 	private final double yen = 1.76;
 	private final double won = 0.19;
 	
@@ -30,23 +30,23 @@ public class DivisaLogic {
 	}
 	
 	//Funcion para convertir de una divisa a pesos
-	public double dolarAPesos(double divisa) {
+	public double dolarAPeso(double divisa) {
 		return divisa*dolar;
 	}
 	
-	public double euroAPesos(double divisa) {
+	public double euroAPeso(double divisa) {
 		return divisa*euro;
 	}
 	
-	public double libraEsterlinaAPesos(double divisa) {
+	public double libraEsterlinaAPeso(double divisa) {
 		return divisa*libraEsterlina;
 	}
 	
-	public double yenAPesos(double divisa) {
+	public double yenAPeso(double divisa) {
 		return divisa*yen;
 	}
 	
-	public double wonAPesos(double divisa) {
+	public double wonAPeso(double divisa) {
 		return divisa*won;
 	}
 }
--- Conversor/src/main/ConversorAlura.java
+++ Conversor/src/main/ConversorAlura.java
@@ -1,8 +1,8 @@
 package main;
 
-import java.awt.EventQueue;
+import javax.swing.JOptionPane;
 
-import gui.Menu;
+import logic.DivisaLogic;
 
 /**
  * 
@@ -12,18 +12,144 @@ import gui.Menu;
  */
 
 public class ConversorAlura {
+	
+	public static void seleccionarTipoDeConversion (String opcion){
+		DivisaLogic divisaLogic = new DivisaLogic();
+		Double inputValue = Double.parseDouble(JOptionPane.showInputDialog(null, "Ingrese el numero a convertir"));
+		
+		switch(opcion) {
+		case "Pesos a Dolares": JOptionPane.showMessageDialog(
+				null,
+				String.format(
+						"El resultado de la conversion es: %.2f dolares",
+						divisaLogic.pesoADolar(inputValue)));
+			break;
+		
+		case "Pesos a Euros": JOptionPane.showMessageDialog(
+				null,
+				String.format(
+						"El resultado de la conversion es: %.2f euros",
+						divisaLogic.pesoAEuro(inputValue)));
+			break;
+		
+		case "Pesos a Libras Esterlinas": JOptionPane.showMessageDialog(
+				null,
+				String.format(
+						"El resultado de la conversion es: %.2f libras esterlinas",
+						divisaLogic.pesoALibraEsterlina(inputValue)));
+			break;
+			
+		case "Pesos a Yen": JOptionPane.showMessageDialog(
+				null,
+				String.format(
+						"El resultado de la conversion es: %.2f yenes",
+						divisaLogic.pesoAYen(inputValue)));
+			break;
+			
+		case "Pesos a Won Coreano": JOptionPane.showMessageDialog(
+				null,
+				String.format(
+						"El resultado de la conversion es: %.2f won coreano",
+						divisaLogic.pesoAWon(inputValue)));
+			break;
+			
+		case "Dolares a Pesos": JOptionPane.showMessageDialog(
+				null,
+				String.format(
+						"El resultado de la conversion es: %.2f pesos",
+						divisaLogic.dolarAPeso(inputValue)));
+			break;
+			
+		case "Euros a Pesos": JOptionPane.showMessageDialog(
+				null,
+				String.format(
+						"El resultado de la conversion es: %.2f pesos",
+						divisaLogic.euroAPeso(inputValue)));
+			break;
+			
+		case "Libras Esterlinas a Pesos": JOptionPane.showMessageDialog(
+				null,
+				String.format(
+						"El resultado de la conversion es: %.2f pesos",
+						divisaLogic.libraEsterlinaAPeso(inputValue)));
+			break;
+			
+		case "Yen a Pesos": JOptionPane.showMessageDialog(
+				null,
+				String.format(
+						"El resultado de la conversion es: %.2f pesos",
+						divisaLogic.yenAPeso(inputValue)));
+			break;
+			
+		case "Won Coreano a Pesos": JOptionPane.showMessageDialog(
+				null,
+				String.format(
+						"El resultado de la conversion es: %.2f pesos",
+						divisaLogic.wonAPeso(inputValue)));
+			break;
+		}
+				
+	}
+		
 
 	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					Menu frame = new Menu();
-					frame.setVisiblePanel(true);
-				} catch (Exception e) {
-					e.printStackTrace();
+		String opcionesDivisas[] = {
+				"Pesos a Dolares",
+				"Pesos a Euros",
+				"Pesos a Libras Esterlinas",
+				"Pesos a Yen",
+				"Pesos a Won Coreano",
+				"Dolares a Pesos",
+				"Euros a Pesos",
+				"Libras Esterlinas a Pesos",
+				"Yen a Pesos",
+				"Won Coreano a Pesos"};
+		String opcionesTemperatura[] = {
+				"Celsius a Farenheit",
+				"Celsius a Kelvin",
+				"Farenheit a Celsius",
+				"Kelvin a Celsius"};
+		String opciones[] = {
+				"Conversor de divisa",
+				"Conversor de temperatura",
+				"Conversor de bit"};
+		String opcion = (String) JOptionPane.showInputDialog(
+				null,
+				"Elija una opcion",
+				"Menu",
+				JOptionPane.DEFAULT_OPTION,
+				null,
+				opciones,
+				opciones[0]);
+		
+		while(opcion != null) {
+			switch(opcion) {
+				case "Conversor de divisa": {
+					opcion = (String) JOptionPane.showInputDialog(
+							null,
+							"Elija una opcion",
+							"Menu",
+							JOptionPane.DEFAULT_OPTION,
+							null,
+							opcionesDivisas,
+							opcionesDivisas[0]);
+					
+					seleccionarTipoDeConversion(opcion);
 				}
+					break;
 			}
-		});
+			
+			opcion = (String) JOptionPane.showInputDialog(
+					null,
+					"Elija una opcion",
+					"Menu",
+					JOptionPane.DEFAULT_OPTION,
+					null,
+					opciones,
+					opciones[0]);
+			
+		}
+		
 	}
 	
 }
